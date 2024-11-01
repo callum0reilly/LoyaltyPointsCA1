5496532df0d49803099f79216150665d4c212bc4
callum0reilly <125283342+callum0reilly@users.noreply.github.com>
2024-11-01 13:12:44 +0000

    Created the Java to compare 2 passwords input by the user. I have also created the SQL database in terminal

--- src/LoyaltyPoints.java
+++ src/LoyaltyPoints.java
@@ -1,17 +1,67 @@
+
+/** Take in neccessary imports from example */
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 public class LoyaltyPoints extends HttpServlet {
     public LoyaltyPoints() {
+
     }
 
+
+    /**
     public static void main(String[] args) {
     }
-//practice commit and push
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+     */
 
-    }
-}
+/** My database plan
+ * Name: loyaltySystem
+ * Table: Users
+ * Columns: Username , Password , PointsTotal
+ * Points Total is automatically set to 100
+ * */
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/loyaltySystem?serverTimezone=UTC","root", "rootroot1"); //my database is called loyaltySystem
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }//end of catch block
+
+        try {
+            String password = request.getParameter("password");
+            String confirmPassword = request.getParameter("confirmPassword");
+
+            if (password.equals(confirmPassword)) { //check if the 2 passwords match
+                PreparedStatement createUser = connection.prepareStatement(
+                        "INSERT into Users (Username, Password, PointsTotal) VALUES (?, ?, ?)");
+                createUser.setString(1, request.getParameter("username"));
+                createUser.setString(2, password);
+                createUser.setInt(3, 100); //sets each inputted user to 100 points
+                createUser.executeUpdate();
+                PrintWriter out = response.getWriter();
+                out.println("You have now successfully registered!");
+            } else {
+                PrintWriter out = response.getWriter();
+                out.println("Passwords do not match");
+            }
+
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        }
+    }//end of doGet method
+}//end of class
