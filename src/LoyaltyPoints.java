5226a84e16274eba96d9b9815fa5052c017ea956
callum0reilly <125283342+callum0reilly@users.noreply.github.com>
2024-11-04 14:42:15 +0000

    This could be my final version. Currently it does not work. I have tried to make the user be able to add and spend points but unfortunately it does not add or remove the points from the database. I will try look at this again later and if i can fix it there will be another version.

--- src/LoyaltyPoints.java
+++ src/LoyaltyPoints.java
@@ -13,6 +13,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import static java.lang.System.out;
+
 
 public class LoyaltyPoints extends HttpServlet {
     public LoyaltyPoints() {
@@ -34,64 +36,129 @@ public class LoyaltyPoints extends HttpServlet {
 
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-
-
         Connection connection = null;
+        PrintWriter out = response.getWriter();
+
         try {
+            // Establish database connection
             connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/loyaltySystem?serverTimezone=UTC","root", "rootroot1"); //my database is called loyaltySystem
+                    "jdbc:mysql://localhost:3306/loyaltySystem?serverTimezone=UTC", "root", "rootroot1");
+            if (connection == null) {
+                out.println("Failed to connect to the database.");
+                return;
+            }
         } catch (SQLException e) {
             e.printStackTrace();
-        }//end of catch block
+            out.println("Database connection error.");
+            return;
+        }
 
         /** Register System */
         try {
             String password = request.getParameter("password");
             String confirmPassword = request.getParameter("confirmPassword");
 
-            if (password.equals(confirmPassword)) { //check if the 2 passwords match
+            if (password != null && password.equals(confirmPassword)) {
                 PreparedStatement createUser = connection.prepareStatement(
-                        "INSERT into Users (Username, Password, PointsTotal) VALUES (?, ?, ?)");
+                        "INSERT INTO Users (Username, Password, PointsTotal) VALUES (?, ?, ?)");
                 createUser.setString(1, request.getParameter("username"));
                 createUser.setString(2, password);
-                createUser.setInt(3, 100); //sets each inputted user to 100 points
+                createUser.setInt(3, 100); // sets each new user to 100 points
                 createUser.executeUpdate();
-                PrintWriter out = response.getWriter();
-                out.println("You have now successfully registered!");
-                out.println("Please refresh the page and login!");
+                out.println("You have now successfully registered! Please refresh the page and log in.");
             } else {
-                PrintWriter out = response.getWriter();
-                out.println("Passwords do not match");
-            }//end of else
-
-        } catch (SQLException e1) {
-            e1.printStackTrace();
-        }//end of catch
-
+                out.println("Passwords do not match or password is null.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            out.println("Error registering user.");
+        }
 
         /** Login System */
-
-        // GET SOMETHING OUT OF THE DATABASE
         try {
             String username = request.getParameter("username");
             String password = request.getParameter("password");
-            PreparedStatement checkUser = connection.prepareStatement(
-                    "SELECT * FROM Users WHERE Username = ? AND Password = ?");
-            checkUser.setString(1, username);
-            checkUser.setString(2, password);
-            PrintWriter out = response.getWriter();
-            ResultSet rs = checkUser.executeQuery();
-            if (rs.next()) {
-                response.sendRedirect("index2.html");
 
+            if (username != null && password != null) {
+                PreparedStatement checkUser = connection.prepareStatement(
+                        "SELECT * FROM Users WHERE Username = ? AND Password = ?");
+                checkUser.setString(1, username);
+                checkUser.setString(2, password);
+                ResultSet rs = checkUser.executeQuery();
+
+                if (rs.next()) {
+                    response.sendRedirect("index2.html");
+                } else {
+                    out.println("User with that username and password combination doesn't exist. Please try again!");
+                }
             } else {
-                out.println("User with that password and username combination dont exist. Please try again!");
+                out.println("Username or password is missing.");
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
-        }//end of catch
+        }
 
-    }//end of doGet method
+        /** Add or Spend Points and Update Them */
+        try {
+            String username = request.getParameter("username");
+            String action = request.getParameter("action");
+            int points;
 
-    }//end of class
+            if (username == null || action == null) {
+                out.println("Missing username or action.");
+                return;
+            }
+
+            if ("addPoints".equals(action)) {
+                points = Integer.parseInt(request.getParameter("addPoints"));
+            } else if ("spendPoints".equals(action)) {
+                points = Integer.parseInt(request.getParameter("removePoints"));
+            } else {
+                out.println("Invalid action. Please select addPoints or spendPoints.");
+                return;
+            }
+
+            PreparedStatement checkPoints = connection.prepareStatement(
+                    "SELECT PointsTotal FROM Users WHERE Username = ?");
+            checkPoints.setString(1, username);
+            ResultSet rs = checkPoints.executeQuery();
+
+            if (rs.next()) {
+                int currentPoints = rs.getInt("PointsTotal");
+                int newPoints;
+
+                if ("spendPoints".equals(action)) {
+                    if (currentPoints >= points) {
+                        newPoints = currentPoints - points;
+                    } else {
+                        out.println("Insufficient points. You have " + currentPoints + " points available.");
+                        return;
+                    }
+                } else {
+                    newPoints = currentPoints + points;
+                }
+
+                PreparedStatement updatePoints = connection.prepareStatement(
+                        "UPDATE Users SET PointsTotal = ? WHERE Username = ?");
+                updatePoints.setInt(1, newPoints);
+                updatePoints.setString(2, username);
+
+                int rowsUpdated = updatePoints.executeUpdate();
+                if (rowsUpdated > 0) {
+                    out.println("Transaction successful. New balance: " + newPoints);
+                } else {
+                    out.println("Error updating points. Please try again.");
+                }
+            } else {
+                out.println("User not found. Please try again.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            out.println("Error updating points.");
+        } catch (NumberFormatException e) {
+            out.println("Invalid points value. Please enter a numeric value.");
+        }
+    }//end of doGet
+
+
+}//end of class
